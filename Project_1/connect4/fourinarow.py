b39a3f633e946aef7b984416ce9d00c23afa0993
Elchin Valiyev <valiyev.elchin@gmail.com>
2016-05-19 19:22:56 +0200

    Presentation slides added

--- Project_1/connect4/fourinarow.py
+++ Project_1/connect4/fourinarow.py
@@ -1,10 +1,9 @@
-import copy
+import matplotlib.pyplot as plt
+import numpy as np
 import pygame
 import random
 import sys
-import numpy as np
 from pygame.locals import *
-import matplotlib.pyplot as plt
 
 BOARDWIDTH = 7  # how many spaces wide the board is
 BOARDHEIGHT = 6  # how many spaces tall the board is
@@ -31,7 +30,7 @@ BLACK = -1
 EMPTY = 0
 HUMAN = 1
 COMPUTER = -1
-lookup =np.zeros((7,6))
+lookup = np.zeros((7, 6))
 
 
 def play_with_ui(agent_1, agent_2, agent_3):
@@ -63,25 +62,25 @@ def play_with_ui(agent_1, agent_2, agent_3):
     ARROWRECT = ARROWIMG.get_rect()
     ARROWRECT.left = REDPILERECT.right + 10
     ARROWRECT.centery = REDPILERECT.centery
-    
+
     red_wins = 0
     black_wins = 0
     tie = 0
     for i in range(10):
-     result,state=agent_3(agent_1, agent_2)
-     if result==1:
-        red_wins +=1 
-     elif result==-1:
-         black_wins +=1
-     elif result==-2: 
-         tie +=1
-     if state==-1 or i==9:
-        pygame.quit()
-        plotResults(red_wins, black_wins, tie)
-        sys.exit()  
+        result, state = agent_3(agent_1, agent_2)
+        if result == 1:
+            red_wins += 1
+        elif result == -1:
+            black_wins += 1
+        elif result == -2:
+            tie += 1
+        if state == -1 or i == 9:
+            pygame.quit()
+            plotResults(red_wins, black_wins, tie)
+            sys.exit()
 
 
-def plotResults(red_wins,black_wins,tie):
+def plotResults(red_wins, black_wins, tie):
     labels = ['Red Wins', 'Black Wins', 'Ties']
     sizes = [red_wins, black_wins, tie]
     colors = ['yellowgreen', 'gold', 'lightskyblue']
@@ -90,17 +89,17 @@ def plotResults(red_wins,black_wins,tie):
     plt.axis('equal')
     plt.tight_layout()
     plt.show()
-    print "number of red wins ",red_wins     
-    print "number of black wins ",black_wins  
-    print "number of ties ",tie
-          
+    print "number of red wins ", red_wins
+    print "number of black wins ", black_wins
+    print "number of ties ", tie
+
 
 def run_random_game(agent_1, agent_2):
     turn = HUMAN
-    winner=0
-    state =0
-    lookup1=np.zeros((7,6))
-    lookup2=np.zeros((7,6))
+    winner = 0
+    state = 0
+    lookup1 = np.zeros((7, 6))
+    lookup2 = np.zeros((7, 6))
     # Set up a blank board data structure.
     mainBoard = getNewBoard()
 
@@ -109,28 +108,28 @@ def run_random_game(agent_1, agent_2):
             # Human player's turn.
             column = agent_1(mainBoard)
             animateComputerMoving(mainBoard, column, HUMAN)
-            status,row=makeMove(mainBoard, RED, column)
+            status, row = makeMove(mainBoard, RED, column)
             if isWinner(mainBoard, RED):
                 winnerImg = HUMANWINNERIMG
-                winner=1
+                winner = 1
                 break
             turn = COMPUTER  # switch to other player's turn
         else:
             # Computer player's turn.
             column = agent_2(mainBoard)
             animateComputerMoving(mainBoard, column, COMPUTER)
-            status,row=makeMove(mainBoard, BLACK, column)
+            status, row = makeMove(mainBoard, BLACK, column)
             # BoardStatus(mainBoard)
             if isWinner(mainBoard, BLACK):
                 winnerImg = COMPUTERWINNERIMG
-                winner=-1
+                winner = -1
                 break
             turn = HUMAN  # switch to other player's turn
 
         if isBoardFull(mainBoard):
             # A completely filled board means it's a tie.
             winnerImg = TIEWINNERIMG
-            winner=-2
+            winner = -2
             break
     while True:
         # Keep looping until player clicks the mouse or quits.
@@ -142,45 +141,45 @@ def run_random_game(agent_1, agent_2):
         for event in pygame.event.get():  # event handling loop
             if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
                 print lookup
-                state=-1
-                return winner,state
+                state = -1
+                return winner, state
             elif event.type == MOUSEBUTTONUP:
-                return winner,state
+                return winner, state
 
 
 def run_learned_game(agent_1, agent_2):
     turn = HUMAN
-    winner=0
-    state =0
-    lookup1=np.zeros((7,6))
-    lookup2=np.zeros((7,6))
+    winner = 0
+    state = 0
+    lookup1 = np.zeros((7, 6))
+    lookup2 = np.zeros((7, 6))
     # Set up a blank board data structure.
     mainBoard = getNewBoard()
 
     while True:  # main game loop
         if turn == HUMAN:
             # Human player's turn.
-            board,column=next_move(mainBoard,1)
+            board, column = next_move(mainBoard, 1)
             animateComputerMoving(mainBoard, column, HUMAN)
             if isWinner(mainBoard, RED):
                 winnerImg = HUMANWINNERIMG
-                winner=1
+                winner = 1
                 break
             turn = COMPUTER  # switch to other player's turn
         else:
             # Computer player's turn.
-            board,column=next_move(mainBoard,-1)
+            board, column = next_move(mainBoard, -1)
             animateComputerMoving(mainBoard, column, COMPUTER)
             if isWinner(mainBoard, BLACK):
                 winnerImg = COMPUTERWINNERIMG
-                winner=-1
+                winner = -1
                 break
             turn = HUMAN  # switch to other player's turn
 
         if isBoardFull(mainBoard):
             # A completely filled board means it's a tie.
             winnerImg = TIEWINNERIMG
-            winner=-2
+            winner = -2
             break
     while True:
         # Keep looping until player clicks the mouse or quits.
@@ -192,19 +191,18 @@ def run_learned_game(agent_1, agent_2):
         for event in pygame.event.get():  # event handling loop
             if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
                 print lookup
-                state=-1
-                return winner,state
+                state = -1
+                return winner, state
             elif event.type == MOUSEBUTTONUP:
-                return winner,state
+                return winner, state
 
-    
 
 def makeMove(board, player, column):
     # Finding the lowest row for the selected column to place the player
     lowest = getLowestEmptySpace(board, column)
     if lowest != -1:
         board[column][lowest] = player
-    return board,lowest
+    return board, lowest
 
 
 def drawBoard(board, extraToken=None):
@@ -241,14 +239,16 @@ def drawBoard(board, extraToken=None):
 
 def getNewBoard():
     # Create a new board and initilize all fields to zero
-    board=np.zeros((BOARDWIDTH,BOARDHEIGHT))
+    board = np.zeros((BOARDWIDTH, BOARDHEIGHT))
     return board
 
+
 def BoardStatus(board):
-    #Returns the currnt board state
+    # Returns the currnt board state
     print board
     print"\n\n"
-    return True    
+    return True
+
 
 def animateDroppingToken(board, column, color):
     x = XMARGIN + column * SPACESIZE
@@ -300,9 +300,9 @@ def animateComputerMoving(board, column, player):
 def getComputerMove(board):
     # pick a random column number which is Valid 
     while True:
-      x=random.randint(0,6)
-      if isValidMove(board,x):
-          return x
+        x = random.randint(0, 6)
+        if isValidMove(board, x):
+            return x
 
 
 def getLowestEmptySpace(board, column):
@@ -312,35 +312,34 @@ def getLowestEmptySpace(board, column):
             return y
     return -1
 
-def next_move(board,player):
+
+def next_move(board, player):
     # getting the next best move by choosing a valid row and col combination which was used the most in lookup  
     valid_column_moves = []
     valid_row_moves = []
     maximum_value = 0
     # getting all the valid column possible moves
     for x in range(7):
-       if isValidMove(board, x):
-           valid_row_moves.append(x) 
-    test=np.load("test_table.npy")
+        if isValidMove(board, x):
+            valid_row_moves.append(x)
+    test = np.load("test_table.npy")
     # # go through all the valid moves and find the one with the largest value 
 
     for i in range(7):
-     lowest = getLowestEmptySpace(board,i)
-     if lowest != -1:
-        valid_column_moves.append(lowest)
-     else: valid_column_moves.append(-1)
-    for y in range (len(valid_column_moves)):
-        if valid_column_moves[y]!=-1:
-           if maximum_value<test[y][valid_column_moves[y]]:
-              maximum_value=test[y][valid_column_moves[y]]
-              max_row = y
-              max_column = valid_column_moves[y]
+        lowest = getLowestEmptySpace(board, i)
+        if lowest != -1:
+            valid_column_moves.append(lowest)
+        else:
+            valid_column_moves.append(-1)
+    for y in range(len(valid_column_moves)):
+        if valid_column_moves[y] != -1:
+            if maximum_value < test[y][valid_column_moves[y]]:
+                maximum_value = test[y][valid_column_moves[y]]
+                max_row = y
+                max_column = valid_column_moves[y]
     # print board          
-    board[max_row][max_column]=player
-    return board,max_column
-
-
-       
+    board[max_row][max_column] = player
+    return board, max_column
 
 
 def isValidMove(board, column):
@@ -358,6 +357,7 @@ def isBoardFull(board):
             if board[x][y] == EMPTY:
                 return False
     return True
+
 
 def isWinner(board, tile):
     # check horizontal spaces
@@ -383,6 +383,7 @@ def isWinner(board, tile):
                         y + 3] == tile:
                 return True
     return False
+
 
 def wasWinningMove(board, tile, pos_x):
     pos_y = getLowestEmptySpace(board, pos_x)
@@ -445,20 +446,20 @@ def play_without_ui(agent_1, agent_2):
     # Set up a blank board data structure.
     board = getNewBoard()
     player = 1
-    lookup1=np.zeros((7,6))
-    lookup2=np.zeros((7,6))
+    lookup1 = np.zeros((7, 6))
+    lookup2 = np.zeros((7, 6))
     while True:  # main game loop
         if player == 1:
             # Human player's turn.
             column = agent_1(board)
-            state,row=makeMove(board, player, column)
+            state, row = makeMove(board, player, column)
             # BoardStatus(board)
-            lookup1[column][row] +=1
+            lookup1[column][row] += 1
         else:
             column = agent_2(board)
-            state,row=makeMove(board, player, column)
+            state, row = makeMove(board, player, column)
             # BoardStatus(board)
-            lookup2[column][row] +=1
+            lookup2[column][row] += 1
         if wasWinningMove(board, player, column):
             build_look(player, lookup1, lookup2)
             return player
@@ -466,24 +467,25 @@ def play_without_ui(agent_1, agent_2):
         if isBoardFull(board):
             # A completely filled board means it's a tie.
             return 0
-        
 
-def build_look(player,lookup1,lookup2):
+
+def build_look(player, lookup1, lookup2):
     # add all the moves of the winning player to build a lookup table
     for x in range(7):
         for y in range(6):
-            if player ==1:   
+            if player == 1:
                 lookup[x][y] += lookup1[x][y]
-            else: 
-                lookup[x][y] += lookup2[x][y] 
+            else:
+                lookup[x][y] += lookup2[x][y]
+
 
 def learn_from_random_play(iterations):
-    #randomly plays the game and saves the lookup table which is the number of times a field was used by the winning player
+    # randomly plays the game and saves the lookup table which is the number of times a field was used by the winning player
     for i in range(iterations):
-      play_without_ui(getComputerMove, getComputerMove) 
-    np.save("test_table.npy",lookup)                 
+        play_without_ui(getComputerMove, getComputerMove)
+    np.save("test_table.npy", lookup)
+
 
 if __name__ == '__main__':
-   #learn_from_random_play(1000)
-   play_with_ui(getComputerMove,getComputerMove,run_learned_game)
-  
+    # learn_from_random_play(1000)
+    play_with_ui(getComputerMove, getComputerMove, run_learned_game)
--- Project_1/tic_tac_toe/Main.py
+++ Project_1/tic_tac_toe/Main.py
@@ -2,7 +2,16 @@ import matplotlib.pyplot as plt
 
 import agent
 import changed_tic_tac_toe as ttt
-from qlearner import QLearner
+
+
+def make_autopct(values):
+    def my_autopct(pct):
+        if pct == 0:
+            return ""
+        else:
+            return '{p:.1f}% '.format(p=pct)
+
+    return my_autopct
 
 
 def draw_chart(winner_list, title):
@@ -10,8 +19,8 @@ def draw_chart(winner_list, title):
     labels = ['X Wins', 'O Wins', 'Draw']
     sizes = [winner_list[1], winner_list[-1], winner_list[0]]
     colors = ['yellowgreen', 'gold', 'lightskyblue']
-    patches, texts = plt.pie(sizes, colors=colors, shadow=True, startangle=90)
-    plt.legend(patches, labels, loc="best")
+    patches = plt.pie(sizes, colors=colors, autopct=make_autopct(sizes), shadow=True, startangle=90)
+    plt.legend(patches[0], labels, loc="best")
     plt.axis('equal')
     plt.tight_layout()
     plt.show()
@@ -27,14 +36,14 @@ def show_stats(agent_1, agent_2, title):
 
 
 if __name__ == '__main__':
-    # random vs probabilistic
-    show_stats(agent.get_random_move, agent.get_probabilistic_move, "Random(X) vs Probabilistic(O)")
+    # random vs random
+    show_stats(agent.get_random_move, agent.get_random_move, "Random(X) vs Random(O)")
 
-    # probabilistic vs minmax
-    show_stats(agent.get_probabilistic_move, agent.get_minmax_move, "Probabilistic(X) vs Minmax(O)")
+    # random vs probabilistic
+    show_stats(agent.get_probabilistic_move, agent.get_random_move, "Probabilistic(X) vs Random(O)")
+
+    # minmax vs random
+    show_stats(agent.get_minmax_move, agent.get_random_move, "Minmax(X) vs Random(O)")
 
     # minmax vs minmax
     show_stats(agent.get_minmax_move, agent.get_minmax_move, "Minmax(X) vs Minmax(O)")
-
-    qlearner = QLearner()
-    show_stats(qlearner.get_max_q_move, agent.get_minmax_move, "Q-learning(X) vs Minmax(O)")
