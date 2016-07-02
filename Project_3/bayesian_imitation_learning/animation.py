79389f7cc30367051f79a9e1cb97a26e7db47301
Mari <mari1rybalka@gmail.com>
2016-07-02 14:56:13 +0200

    added soms with faulty animation

--- Project_3/bayesian_imitation_learning/animation.py
+++ Project_3/bayesian_imitation_learning/animation.py
@@ -17,18 +17,9 @@ with open("q3dm1-path2.csv") as filein:
     reader = csv.reader(filein, quoting=csv.QUOTE_NONNUMERIC, skipinitialspace=True)
     xs, ys, zs = zip(*reader)
 
-map_input = np.array([xs,ys,zs]).transpose()
-
-
-
-
-
-
+map_input = np.array([xs, ys, zs]).transpose()
 
 fig = plt.figure()
-
-
-
 
 xa = som.neurons[:, :, 0].flatten()
 ya = som.neurons[:, :, 1].flatten()
@@ -39,7 +30,7 @@ ax = plt.axes(projection='3d')
 ax.scatter(xa, ya, za)
 
 
-#im = plt.imshow(ax, cmap=plt.get_cmap('viridis'), animated=True)
+# im = plt.imshow(ax, cmap=plt.get_cmap('viridis'), animated=True)
 def update_som(som):
     for i in range(len(map_input)):
         som.activate(map_input[i])
@@ -47,20 +38,17 @@ def update_som(som):
     return som
 
 
-
-
-def updatefig(i,som,ax):
+def updatefig(i, som, ax):
     som.activate(map_input[i])
     som.backward()
     xa = som.neurons[:, :, 0].flatten()
     ya = som.neurons[:, :, 1].flatten()
     za = som.neurons[:, :, 2].flatten()
-    sc = ax.scatter(xa,ya,za)
+    sc = ax.scatter(xa, ya, za)
     return sc,
 
 
 numframes = 10
-
 
 ani = animation.FuncAnimation(fig, updatefig, frames=xrange(numframes), fargs=(som, ax))
 plt.show()
--- Project_3/bayesian_imitation_learning/bayesian_test.py
+++ Project_3/bayesian_imitation_learning/bayesian_test.py
@@ -4,15 +4,14 @@ from kohonen.kohonen import *
 import numpy as np
 import csv
 
-
 with open("q3dm1-path2.csv") as filein:
     # quoting - is needed for conversion to int, since csv reads as strings
     reader = csv.reader(filein, quoting=csv.QUOTE_NONNUMERIC, skipinitialspace=True)
     xs, ys, zs = zip(*reader)
 
-input = np.array([xs,ys,zs]).transpose()
+input = np.array([xs, ys, zs]).transpose()
 
-som_parameters = Parameters(dimension=3,shape=(1,40),learning_rate=0.1,neighborhood_size=2,metric=euclidean_metric)
+som_parameters = Parameters(dimension=3, shape=(1, 40), learning_rate=0.1, neighborhood_size=2, metric=euclidean_metric)
 som = Map(som_parameters)
 print len(input)
 for i in range(len(input)):
@@ -20,16 +19,5 @@ for i in range(len(input)):
 
 print som.neurons
 plt.figure()
-Map.neuron_heatmap(som,axes=(0,1))
+Map.neuron_heatmap(som, axes=(0, 1))
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
